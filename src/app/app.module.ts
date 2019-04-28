c003df4e93ae4397e1ab81d7dbfb29b1be85b416
l08084 <l08084.1989@gmail.com>
2019-04-28 17:23:46 +0900

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
+import { KintaiCardComponent } from './component/kintai-card/kintai-card.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, KintaiCardComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
---
+++ src/app/component/kintai-card/kintai-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kintai-card',
+  templateUrl: './kintai-card.component.html',
+  styleUrls: ['./kintai-card.component.scss']
+})
+export class KintaiCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

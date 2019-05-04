7d28305ff3f0444cca38cb16757226a525a4752d
l08084 <l08084.1989@gmail.com>
2019-05-04 21:42:45 +0900

    add kintai-table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { KintaiCardComponent } from './component/kintai-card/kintai-card.component';
+import { KintaiTableComponent } from './component/kintai-table/kintai-table.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, KintaiCardComponent],
+  declarations: [AppComponent, HeaderComponent, KintaiCardComponent, KintaiTableComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -24,6 +26,7 @@ import { KintaiCardComponent } from './component/kintai-card/kintai-card.compone
     MatFormFieldModule,
     FormsModule,
     MatToolbarModule,
+    MatTableModule,
     MatIconModule,
     MatSelectModule,
     MatButtonModule,
---
+++ src/app/component/kintai-table/kintai-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kintai-table',
+  templateUrl: './kintai-table.component.html',
+  styleUrls: ['./kintai-table.component.scss']
+})
+export class KintaiTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

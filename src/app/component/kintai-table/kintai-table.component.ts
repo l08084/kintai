860dec92082346599a5ff77add431c918965cbf0
l08084 <l08084.1989@gmail.com>
2019-05-12 16:31:56 +0900

    update table

--- src/app/component/kintai-table/kintai-table.component.ts
+++ src/app/component/kintai-table/kintai-table.component.ts
@@ -1,23 +1,15 @@
 import { Component } from '@angular/core';
+import { KintaiEntity } from 'src/app/entity/kintai.entity';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' }
+const ELEMENT_DATA: KintaiEntity[] = [
+  {
+    position: 1,
+    name: '2019/5/12',
+    workTime: 8.0,
+    start: '9:00',
+    end: '18:00',
+    breakTime: '1.0'
+  }
 ];
 
 @Component({
@@ -26,6 +18,13 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./kintai-table.component.scss']
 })
 export class KintaiTableComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = [
+    'position',
+    'name',
+    'workTime',
+    'start',
+    'end',
+    'breakTime'
+  ];
   dataSource = ELEMENT_DATA;
 }
---
+++ src/app/entity/kintai.entity.ts
@@ -0,0 +1,8 @@
+export interface KintaiEntity {
+  name: string;
+  position: number;
+  workTime: number;
+  start: string;
+  end: string;
+  breakTime: string;
+}

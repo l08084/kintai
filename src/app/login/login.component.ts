615908a8f43f5d54f33a5a786c67e7f353d723d5
l08084 <l08084.1989@gmail.com>
2019-08-18 18:58:32 +0900

    add comment

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,14 +6,24 @@ import {
   FormBuilder
 } from '@angular/forms';
 
+/**
+ * ログイン画面コンポーネント
+ *
+ * @export
+ * @class LoginComponent
+ * @implements {OnInit}
+ */
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  // FormGroup定義
   public loginFormGroup: FormGroup;
+  // emailフォームのコントロール定義
   public emailControl: FormControl;
+  // passwordフォームのコントロール定義
   public passwordControl: FormControl;
 
   constructor(private fb: FormBuilder) {}
@@ -24,12 +34,20 @@ export class LoginComponent implements OnInit {
     this.passwordControl = this.loginFormGroup.get('password') as FormControl;
   }
 
+  /**
+   * ログインボタン押下時に呼び出し
+   *
+   */
   public onSubmit() {
     console.log(this.loginFormGroup.value);
     console.log(this.emailControl.value);
     console.log(this.passwordControl.value);
   }
 
+  /**
+   * Eメールフォームにバリデーションエラーメッセージを表示
+   *
+   */
   public getErrorMessageToEmail() {
     return this.emailControl.hasError('required')
       ? 'You must enter a value'
@@ -38,12 +56,20 @@ export class LoginComponent implements OnInit {
       : '';
   }
 
+  /**
+   * パスワードフォームにバリデーションエラーメッセージを表示
+   *
+   */
   public getErrorMessageToPassword() {
     return this.passwordControl.hasError('required')
       ? 'You must enter a value'
       : '';
   }
 
+  /**
+   * フォーム設定の作成
+   *
+   */
   private createForm() {
     this.loginFormGroup = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
